bd47f028972323d4fcef407de6888b929019ac24
ling83-ft <feeling_tandiono@yahoo.com>
2025-04-07 14:48:38 +0200

    commit

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -4,10 +4,22 @@ import ReactAnimatedWeather from "react-animated-weather";
 export default function WeatherIcon(props) {
   const codeMapping = {
     "clear-sky-day": "CLEAR_DAY",
+    "clear-sky-night": "CLEAR_NIGHT",
     "few-clouds-day": "PARTLY_CLOUDY_DAY",
+    "few-clouds-night": "PARTLY_CLOUDY_NIGHT",
     "broken-clouds-day": "CLOUDY",
     "scattered-clouds-day": "CLOUDY",
+    "scattered-clouds-night": "CLOUDY",
     "rain-day": "RAIN",
+    "rain-night": "RAIN",
+    "shower-rain-day": "RAIN",
+    "shower-rain-night": "RAIN",
+    "snow-day": "SNOW",
+    "snow-night": "SNOW",
+    "mist-day": "FOG",
+    "mist-night": "FOG",
+    "thunderstorm-day": "RAIN",
+    "thunderstorm-night": "RAIN",
   };
 
   return (
